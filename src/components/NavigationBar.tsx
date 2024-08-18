d7dddf270a472de74721aba97b1d78f5e269b58d
bagas <bagasab11@gmail.com>
2024-08-18 17:11:26 +0700

    fix size navbar & project img

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -36,7 +36,7 @@ const NavigationBar = () => {
 
   return (
     <nav className="w-full flex justify-center relative z-[99999]">
-      <ul className=" font-extrabold  text-[#F5F5F5] flex justify-center gap-10 text-3xl z-50 fixed top-0 bg-red-700 m-0-auto py-2 lg:w-2/6 w-5/6 rounded-b-full shadow-custom max-lg:hidden">
+      <ul className=" font-extrabold  text-[#F5F5F5] flex justify-center gap-10 text-2xl z-50 fixed top-0 bg-red-700 m-0-auto py-2 lg:w-2/6 w-5/6 h-[3rem] rounded-b-full shadow-custom max-lg:hidden">
         <Tooltip showArrow content={'Home'} className="navbar-animated">
           <li className="px-4 hover:cursor-pointer" onClick={() => scrollToSection('section1')}>
             <FaHouseChimney />
