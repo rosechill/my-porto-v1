fb08bd5956559d9d63d0dabb90d4cb6cee4fe65e
Bagas Adytia <bagasab11@gmail.com>
2025-11-09 17:39:20 +0700

    fix: handle animation when the layout ready

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,29 +4,17 @@ import ImageHero from './ImageHero'
 import gsap from 'gsap'
 
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { useLayoutEffect, useRef, useEffect } from 'react'
+import { useLayoutEffect, useRef, useEffect, useState } from 'react'
 import { InfiniteMovingCardsDemo } from './containerMovingIcon'
+import React from 'react'
 
 const index = () => {
   const backgroundImage = useRef(null)
   const landingImage = useRef(null)
 
-  useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger)
+  const [ready, setReady] = useState(false)
 
-    const timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: document.documentElement,
-        start: 'top',
-        end: '+=500px',
-        scrub: true,
-      },
-    })
-
-    timeline.from(backgroundImage.current, { clipPath: 'inset(0)' }).to(landingImage.current, { height: '400px' }, 0)
-  }, [])
-
-  useEffect(() => {
+  const doAnimation = () => {
     gsap.set('.transition-image', { scale: 1 })
 
     gsap.fromTo(
@@ -42,9 +30,7 @@ const index = () => {
         opacity: 1,
       }
     )
-  }, [])
 
-  useEffect(() => {
     gsap.set('.text-container', { opacity: 0 })
 
     gsap.fromTo(
@@ -61,9 +47,7 @@ const index = () => {
         x: 0,
       }
     )
-  }, [])
 
-  useEffect(() => {
     gsap.set('.text-container2', { opacity: 0 })
 
     gsap.fromTo(
@@ -80,9 +64,7 @@ const index = () => {
         x: 0,
       }
     )
-  }, [])
 
-  useEffect(() => {
     gsap.fromTo(
       '.section1',
       {
@@ -94,7 +76,97 @@ const index = () => {
         ease: 'power4.inOut',
       }
     )
+  }
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 'top',
+        end: '+=500px',
+        scrub: true,
+      },
+    })
+
+    timeline.from(backgroundImage.current, { clipPath: 'inset(0)' }).to(landingImage.current, { height: '400px' }, 0)
+    setReady(true)
   }, [])
+
+  useEffect(() => {
+    if (ready) {
+      doAnimation()
+    }
+  }, [ready])
+
+  const content = () => {
+    return (
+      <React.Fragment>
+        {/* desktop */}
+        <div ref={landingImage} className="w-full h-full flex justify-center z-50 max-lg:hidden ">
+          <div className="w-full relative  m-[5rem] border-4  border-red-700 shadow-2xl flex justify-center bg-[#ffe7e7] rounded-3xl ">
+            {ready && (
+              <React.Fragment>
+                <div className=" w-full h-full flex flex-col gap-5 z-10 justify-center ms-5 text-container">
+                  <h2 className="lg:text-3xl text-lg text-red-700">Hay, i'm</h2>
+                  <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Bagas Adytia</h2>
+                  <Socmed />
+                </div>
+                <div className=" w-full h-full flex flex-col gap-5 z-10 items-end justify-center me-5 text-container2">
+                  <h2 className="lg:text-3xl text-lg text-red-700">Passionate</h2>
+                  <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Front-End</h2>
+                  <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Developer</h2>
+                </div>
+              </React.Fragment>
+            )}
+          </div>
+          {ready && <ImageHero />}
+        </div>
+
+        {ready && (
+          <div className="transition-image w-[480px] m-0-auto z-50 absolute bottom-0 max-lg:hidden">
+            <InfiniteMovingCardsDemo />
+          </div>
+        )}
+
+        {/* mobile */}
+        <div className=" w-full h-full flex flex-col z-50 lg:hidden ">
+          <div className="flex flex-col pt-24 text-center gap-4">
+            <h2 className="lg:text-3xl text-lg text-red-700">Hay, i'm </h2>
+            <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Bagas Adytia</h2>
+          </div>
+          <div className="flex  flex-col justify-center">
+            <ImageHero />
+            <Socmed />
+          </div>
+          <div>
+            <InfiniteMovingCardsDemo />
+          </div>
+          <div className="flex flex-col text-center gap-4">
+            <h2 className="lg:text-3xl text-lg text-red-700">Passionate</h2>
+            <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Front-End Developer</h2>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
+
+  const stylingContent = () => {
+    return (
+      <React.Fragment>
+        <div className="w-full h-[3rem] absolute bg-red-700 bottom-[50%] max-lg:hidden "></div>
+        <div className="w-full h-[3rem] absolute bg-red-700  top-0  max-lg:hidden "></div>
+        <div className="w-full h-[3rem] absolute bg-red-700  bottom-0  "></div>
+
+        <div className="absolute w-[45%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 left-0 -skew-y-[100deg]"></div>
+        <div className="absolute w-[30%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 left-0 -skew-y-[100deg]"></div>
+
+        <div className="absolute w-[45%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 right-0 skew-y-[100deg]"></div>
+        <div className="absolute w-[30%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 right-0 skew-y-[100deg]"></div>
+      </React.Fragment>
+    )
+  }
 
   return (
     <div
@@ -102,55 +174,8 @@ const index = () => {
       ref={backgroundImage}
       className="relative h-screen w-full flex justify-center items-center bg-[#F5F5F5] overflow-hidden"
     >
-      {/* desktop */}
-      <div ref={landingImage} className="w-full h-full flex justify-center z-50 max-lg:hidden ">
-        <div className="w-full relative  m-[5rem] border-4  border-red-700 shadow-2xl flex justify-center bg-[#ffe7e7] rounded-3xl ">
-          <div className=" w-full h-full flex flex-col gap-5 z-10 justify-center ms-5 text-container">
-            <h2 className="lg:text-3xl text-lg text-red-700">Hay, i'm</h2>
-            <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Bagas Adytia</h2>
-            <Socmed />
-          </div>
-          <div className=" w-full h-full flex flex-col gap-5 z-10 items-end justify-center me-5 text-container2">
-            <h2 className="lg:text-3xl text-lg text-red-700">Passionate</h2>
-            <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Front-End</h2>
-            <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Developer</h2>
-          </div>
-        </div>
-        <ImageHero />
-      </div>
-      <div className="transition-image w-[480px] m-0-auto z-50 absolute bottom-0 max-lg:hidden">
-        <InfiniteMovingCardsDemo />
-      </div>
-
-      {/* mobile */}
-      <div className=" w-full h-full flex flex-col z-50 lg:hidden ">
-        <div className="flex flex-col pt-24 text-center gap-4">
-          <h2 className="lg:text-3xl text-lg text-red-700">Hay, i'm </h2>
-          <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Bagas Adytia</h2>
-        </div>
-        <div className="flex  flex-col justify-center">
-          <ImageHero />
-          <Socmed />
-        </div>
-        <div>
-          <InfiniteMovingCardsDemo />
-        </div>
-        <div className="flex flex-col text-center gap-4">
-          <h2 className="lg:text-3xl text-lg text-red-700">Passionate</h2>
-          <h2 className="lg:text-6xl text-2xl font-extrabold text-red-700">Front-End Developer</h2>
-        </div>
-      </div>
-
-      {/* styling */}
-      <div className="w-full h-[3rem] absolute bg-red-700 bottom-[50%] max-lg:hidden "></div>
-      <div className="w-full h-[3rem] absolute bg-red-700  top-0  max-lg:hidden "></div>
-      <div className="w-full h-[3rem] absolute bg-red-700  bottom-0  "></div>
-
-      <div className="absolute w-[45%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 left-0 -skew-y-[100deg]"></div>
-      <div className="absolute w-[30%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 left-0 -skew-y-[100deg]"></div>
-
-      <div className="absolute w-[45%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 right-0 skew-y-[100deg]"></div>
-      <div className="absolute w-[30%] h-[7rem] bg-[#F5F5F5] max-lg:hidden bottom-0 right-0 skew-y-[100deg]"></div>
+      {content()}
+      {stylingContent()}
     </div>
   )
 }
