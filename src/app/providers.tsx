4db440f8f88ae83c199cba68bb689c5a923de92f
Bagas Adytia <v.bagas.budianto@banksaqu.co.id>
2026-05-07 19:05:45 +0700

    feat: add vercel analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,11 +3,13 @@
 
 import { NextUIProvider } from '@nextui-org/react'
 import { ToastContainer } from 'react-toastify'
+import { Analytics } from '@vercel/analytics/next'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
       {children}
+      <Analytics />
       <ToastContainer />
     </NextUIProvider>
   )
